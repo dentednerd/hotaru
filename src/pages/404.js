d4c0d274695af65ec9f7fef5e7b14d68d818f30a
Jo Imlay <jo.imlay@gmail.com>
2019-12-01 15:03:22 +0000

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,13 @@ import Layout from '../templates/Layout';
 const Styled404 = styled('div')`
   height: 50vh;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  a {
+    color: #ffffff;
+  }
 `;
 
 export default () => (
@@ -16,6 +20,8 @@ export default () => (
     <Screen className="purple" noIcon>
       <Styled404>
         <h1>Quoth the server, 404.</h1>
+        <a href="/"><p>home</p></a>
+        <a href="/journal"><p>journal</p></a>
       </Styled404>
     </Screen>
   </Layout>
