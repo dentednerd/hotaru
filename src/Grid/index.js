03b661eb29ac6be0526137e5034731cac0737b52
Joey Imlay <jo.imlay@gmail.com>
2020-02-05 16:11:19 +0000

    updated Grid styling for mobile

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,22 +1,36 @@
 import styled from '@emotion/styled';
 
 const Grid = styled('div')`
-  display: grid;
-  height: 80vh;
-  width: 80vh;
-  grid-template-rows: repeat(16, 1fr);
-  grid-template-columns: repeat(16, 1fr);
-  grid-gap: 1rem;
+  display: flex;
+  flex-flow: column nowrap;
+
+  @media (min-width: 1024px) {
+    display: grid;
+    height: 80vh;
+    width: 80vh;
+    grid-template-rows: repeat(16, 1fr);
+    grid-template-columns: repeat(16, 1fr);
+    grid-gap: 1rem;
+  }
 `;
 
 const Main = styled('a')`
   display: block;
-  grid-row: span 8;
-  grid-column: span 16;
   background-size: cover;
   background-position: center center;
   transition: all .2s ease-in-out;
   box-shadow: 0.1rem 0.1rem 0.25rem #332e4a;
+  width: 80vw;
+  height: 45vw;
+  margin-bottom: 2rem;
+
+  @media (min-width: 1024px) {
+    grid-row: span 8;
+    grid-column: span 16;
+    width: auto;
+    height: auto;
+    margin-bottom: 0;
+  }
 
   &:hover {
     transform: scale(1.1);
@@ -26,12 +40,20 @@ const Main = styled('a')`
 
 const Tall = styled('a')`
   display: block;
-  grid-row: span 16;
-  grid-column: span 8;
   background-size: cover;
   background-position: center center;
   transition: all .2s ease-in-out;
   box-shadow: 0.1rem 0.1rem 0.25rem #332e4a;
+
+  width: 18vh;
+  height: 32vh;
+  margin: 0 auto 2rem;
+
+  @media (min-width: 1024px) {
+    grid-row: span 16;
+    grid-column: span 8;
+    margin: 0;
+  }
 
   &:hover {
     transform: scale(1.1);
@@ -40,21 +62,30 @@ const Tall = styled('a')`
 `;
 
 const Square = styled('div')`
-  grid-row: span 8;
-  grid-column: span 8;
-  background-size: contain;
-  background-position: center center;
+  display: none;
+
+  @media (min-width: 1024px) {
+    grid-row: span 8;
+    grid-column: span 8;
+    background-size: contain;
+    background-position: center center;
+  }
 `;
 
 const Wide = styled('div')`
-  grid-row: span 4;
-  grid-column: span 8;
   box-shadow: none;
   display: flex;
   flex-flow: column wrap;
   justify-content: center;
-  align-items: right;
-  text-align: right;
+  align-items: center;
+  text-align: center;
+
+  @media (min-width: 1024px) {
+    grid-row: span 4;
+    grid-column: span 8;
+    align-items: right;
+    text-align: right;
+  }
 
   &:hover {
     transform: scale(1);
