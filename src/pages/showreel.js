f0960f14334dbe67d2218d06d00c5067227cd2ab
Joey Imlay <jo.imlay@gmail.com>
2020-07-06 18:43:10 +0100

    refactored Showreel, added bg illustrations

--- src/pages/showreel.js
+++ src/pages/showreel.js
@@ -1,83 +1,62 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useRef } from 'react';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 import Screen from '../organisms/Screen';
 import Layout from '../templates/Layout';
 import showreel from '../templates/Showreel';
 import Hero from '../organisms/Hero';
+import * as illustrations from '../assets';
+import { classnames } from '../helpers';
 import './global.css';
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.topSection = React.createRef();
-    this.section0 = React.createRef();
-    this.section1 = React.createRef();
-    this.section2 = React.createRef();
-    this.section3 = React.createRef();
-    this.section4 = React.createRef();
-    this.section5 = React.createRef();
-    this.scroll = this.scroll.bind(this);
-  }
+const illusArray = Object.values(illustrations);
 
-  scroll(ref) { // eslint-disable-line class-methods-use-this
+const Showreel = () => {
+  const topSection = useRef();
+  const section0 = useRef();
+  const section1 = useRef();
+  const section2 = useRef();
+  const section3 = useRef();
+  const section4 = useRef();
+  const section5 = useRef();
+
+  const refs = [
+    section0,
+    section1,
+    section2,
+    section3,
+    section4,
+    section5,
+  ];
+
+  const scroll = (ref) => { // eslint-disable-line class-methods-use-this
     ref.current.scrollIntoView({ behavior: 'smooth' });
-  }
+  };
 
-  render() {
-    return (
-      <Layout>
-        <Hero
-          text="showreel"
-          passedRef={this.topSection}
-          passedNextRef={this.section0}
-          icon={faYoutube}
-          scrollFunc={this.scroll}
-        />
+  return (
+    <Layout>
+      
+      <Hero
+        text="showreel"
+        passedRef={topSection}
+        passedNextRef={section0}
+        icon={faYoutube}
+        scrollFunc={scroll}
+      />
+
+      {showreel.map((item, index) => (
         <Screen
-          className="sage"
-          ref={this.section0}
-          onClick={() => this.scroll(this.section1)}
+          className={classnames[index % 4]}
+          ref={refs[index]}
+          onClick={() => scroll(index === showreel.length - 1 ? topSection : refs[index + 1])}
+          background={illusArray[index]}
+          backToTop={index === showreel.length - 1}
         >
-          {showreel[0]}
+          {item}
         </Screen>
-        <Screen
-          className="lemon"
-          ref={this.section1}
-          onClick={() => this.scroll(this.section2)}
-        >
-          {showreel[1]}
-        </Screen>
-        <Screen
-          className="sky"
-          ref={this.section2}
-          onClick={() => this.scroll(this.section3)}
-        >
-          {showreel[2]}
-        </Screen>
-        <Screen
-          className="sage"
-          ref={this.section3}
-          onClick={() => this.scroll(this.section4)}
-        >
-          {showreel[3]}
-        </Screen>
-        <Screen
-          className="lemon"
-          ref={this.section4}
-          onClick={() => this.scroll(this.section5)}
-        >
-          {showreel[4]}
-        </Screen>
-        <Screen
-          className="sky"
-          backToTop
-          ref={this.section5}
-          onClick={() => this.scroll(this.topSection)}
-        >
-          {showreel[5]}
-        </Screen>
-      </Layout>
-    );
-  }
+      ))}
+
+    </Layout>
+  );
 }
+
+export default Showreel;
